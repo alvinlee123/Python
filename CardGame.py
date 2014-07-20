61e02e888e60f41f27c0c9e0193e825255482eee
alvinlee123 <alvinlee123@hotmail.com>
2014-07-19 20:45:08 -0700

    Created CardGame class

---
+++ CardGame.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 19 14:09:51 2014
+
+@author: alvin
+"""
+
+class Card():
+    def __init(self,num,suit):
+        self.number=num
+        self.suit=suit
+        
